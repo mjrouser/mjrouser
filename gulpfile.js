ec695ecb0d3a6453103cef35c8b58c54bb94683e
Matthew R <matthew@mjrouser.com>
2017-11-03 17:27:10 -0400

    gulp build tool has basic configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    //sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cssnano = require('gulp-cssnano'),
     jshint = require('gulp-jshint'),
@@ -11,45 +11,51 @@ var gulp = require('gulp'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
     del = require('del');
+    htmlmin = require('gulp-htmlmin');
 
+
+gulp.task('html', function() {
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/'))
+    .pipe(notify({ message: 'html task complete' }));
+});
 
 gulp.task('styles', function() {
-  return sass('src/styles/main.scss', { style: 'expanded' })
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(cssnano())
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    return gulp.src('src/styles/main.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/src/styles'))
+        .pipe(notify({ message: 'Styles task complete' }));
 });
+
 
 gulp.task('scripts', function() {
   return gulp.src('src/scripts/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(gulp.dest('dist/src/js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(gulp.dest('dist/src/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 gulp.task('images', function() {
   return gulp.src('src/img/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/assets/img'))
+    .pipe(gulp.dest('dist/src/img'))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('clean', function() {
-    return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
+    return del(['dist/src/css', 'dist/src/js', 'dist/src/img']);
 });
 
 gulp.task('watch', function() {
 
   // Watch .scss files
-  gulp.watch('src/styles/**/*.scss', ['styles']);
+  gulp.watch('src/styles/**/*.css', ['styles']);
 
   // Watch .js files
   gulp.watch('src/scripts/**/*.js', ['scripts']);
@@ -65,6 +71,8 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('default', ['clean'] , function() {
-    gulp.start('styles', 'scripts', 'images');
+gulp.task('build', ['clean','html', 'styles', 'scripts', 'images']);
+
+gulp.task('default', ['connect', 'watch'], function(){
+   return gutil.log('Gulp is running!')
 });
